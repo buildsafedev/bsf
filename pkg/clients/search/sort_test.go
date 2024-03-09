2a9a832d1503dc582779404c61b06eef40dc09fb
horiodino <holiodin@gmail.com>
2024-03-09 13:07:49 +0530

    added: testcase
    Signed-off-by: horiodino <holiodin@gmail.com>

--- pkg/clients/search/sort_test.go
+++ pkg/clients/search/sort_test.go
@@ -1,9 +1,12 @@
 package search
 
 import (
+	"fmt"
 	"testing"
 
 	buildsafev1 "github.com/buildsafedev/bsf-apis/go/buildsafe/v1"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
 func TestSortPackages(t *testing.T) {
@@ -13,19 +16,52 @@ func TestSortPackages(t *testing.T) {
 		want []*buildsafev1.Package
 	}{
 		{
-			name: "Test Case 1",
+			name: "Test case for semver compliant packages",
 			pkgs: []*buildsafev1.Package{
 				{
-					Name:         "test",
+					Name:         "semver",
 					Version:      "1.0.0",
 					SpdxId:       "MIT",
 					Free:         true,
 					Homepage:     "https://test.com",
 					EpochSeconds: 1,
-				},
-				{
-					Name:         "test",
+				}, {
+					Name:         "semver",
 					Version:      "2.0.0",
+					SpdxId:       "MIT",
+					Free:         true,
+					Homepage:     "https://test.com",
+					EpochSeconds: 2,
+				}, {Name: "semver",
+					Version:      "1.5.6",
+					SpdxId:       "MIT",
+					Free:         true,
+					Homepage:     "https://test.com",
+					EpochSeconds: 2,
+				}, {
+					Name:         "semver",
+					Version:      "0.3.0",
+					SpdxId:       "MIT",
+					Free:         true,
+					Homepage:     "https://test.com",
+					EpochSeconds: 2,
+				}, {
+					Name:         "semver",
+					Version:      "11.6.0",
+					SpdxId:       "MIT",
+					Free:         true,
+					Homepage:     "https://test.com",
+					EpochSeconds: 2,
+				}, {
+					Name:         "semver",
+					Version:      "2.11.6",
+					SpdxId:       "MIT",
+					Free:         true,
+					Homepage:     "https://test.com",
+					EpochSeconds: 2,
+				}, {
+					Name:         "semver",
+					Version:      "2.6.9",
 					SpdxId:       "MIT",
 					Free:         true,
 					Homepage:     "https://test.com",
@@ -34,44 +70,77 @@ func TestSortPackages(t *testing.T) {
 			},
 			want: []*buildsafev1.Package{
 				{
-					Name:         "test",
-					Version:      "2.0.0",
+					Name:         "semver",
+					Version:      "11.6.0",
 					SpdxId:       "MIT",
 					Free:         true,
 					Homepage:     "https://test.com",
 					EpochSeconds: 2,
 				},
 				{
-					Name:         "test",
+					Name:         "semver",
+					Version:      "2.11.6",
+					SpdxId:       "MIT",
+					Free:         true,
+					Homepage:     "https://test.com",
+					EpochSeconds: 2,
+				}, {
+					Name:         "semver",
+					Version:      "2.6.9",
+					SpdxId:       "MIT",
+					Free:         true,
+					Homepage:     "https://test.com",
+					EpochSeconds: 2,
+				}, {
+					Name:         "semver",
+					Version:      "2.0.0",
+					SpdxId:       "MIT",
+					Free:         true,
+					Homepage:     "https://test.com",
+					EpochSeconds: 2,
+				}, {
+					Name:         "semver",
+					Version:      "1.5.6",
+					SpdxId:       "MIT",
+					Free:         true,
+					Homepage:     "https://test.com",
+					EpochSeconds: 2,
+				}, {
+					Name:         "semver",
 					Version:      "1.0.0",
 					SpdxId:       "MIT",
 					Free:         true,
 					Homepage:     "https://test.com",
 					EpochSeconds: 1,
+				}, {
+					Name:         "semver",
+					Version:      "0.3.0",
+					SpdxId:       "MIT",
+					Free:         true,
+					Homepage:     "https://test.com",
+					EpochSeconds: 2,
 				},
 			},
 		},
 		{
-			name: "Test Case 2",
+			name: "Test case semver and non-semver packages",
 			pkgs: []*buildsafev1.Package{
 				{
-					Name:         "test",
+					Name:         "non-semver",
 					Version:      "234ca.b243.cc32c",
 					SpdxId:       "MIT",
 					Free:         true,
 					Homepage:     "https://test.com",
 					EpochSeconds: 1,
-				},
-				{
-					Name:         "test",
+				}, {
+					Name:         "non-semver",
 					Version:      "3213.12212.1212",
 					SpdxId:       "MIT",
 					Free:         true,
 					Homepage:     "https://test.com",
 					EpochSeconds: 1,
-				},
-				{
-					Name:         "test",
+				}, {
+					Name:         "semver",
 					Version:      "1.5.22",
 					SpdxId:       "MIT",
 					Free:         true,
@@ -81,7 +150,7 @@ func TestSortPackages(t *testing.T) {
 			},
 			want: []*buildsafev1.Package{
 				{
-					Name:         "test",
+					Name:         "non-semver",
 					Version:      "234ca.b243.cc32c",
 					SpdxId:       "MIT",
 					Free:         true,
@@ -89,7 +158,7 @@ func TestSortPackages(t *testing.T) {
 					EpochSeconds: 1,
 				},
 				{
-					Name:         "test",
+					Name:         "non-semver",
 					Version:      "3213.12212.1212",
 					SpdxId:       "MIT",
 					Free:         true,
@@ -97,12 +166,90 @@ func TestSortPackages(t *testing.T) {
 					EpochSeconds: 1,
 				},
 				{
-					Name:         "test",
+					Name:         "semver",
 					Version:      "1.5.22",
 					SpdxId:       "MIT",
 					Free:         true,
 					Homepage:     "https://test.com",
 					EpochSeconds: 1,
+				},
+			},
+		}, {
+			name: " Test case SortPackagesWithTimestamp",
+			pkgs: []*buildsafev1.Package{
+				{
+					Name:         "non-semver",
+					Version:      "32fd.12a12.1212",
+					SpdxId:       "MIT",
+					Free:         true,
+					Homepage:     "https://test.com",
+					EpochSeconds: 10,
+				}, {
+					Name:         "non-semver",
+					Version:      "4fd2.1212.1212",
+					SpdxId:       "MIT",
+					Free:         true,
+					Homepage:     "https://test.com",
+					EpochSeconds: 4,
+				}, {
+					Name:         "non-semver",
+					Version:      "5fd2.1212.1212",
+					SpdxId:       "MIT",
+					Free:         true,
+					Homepage:     "https://test.com",
+					EpochSeconds: 2,
+				}, {
+					Name:         "non-semver",
+					Version:      "6343.4r32.1212",
+					SpdxId:       "MIT",
+					Free:         true,
+					Homepage:     "https://test.com",
+					EpochSeconds: 3,
+				}, {
+					Name:         "non-semver",
+					Version:      "21d2.1212.1212",
+					SpdxId:       "MIT",
+					Free:         true,
+					Homepage:     "https://test.com",
+					EpochSeconds: 12,
+				},
+			},
+			want: []*buildsafev1.Package{
+				{
+					Name:         "non-semver",
+					Version:      "21d2.1212.1212",
+					SpdxId:       "MIT",
+					Free:         true,
+					Homepage:     "https://test.com",
+					EpochSeconds: 12,
+				}, {
+					Name:         "non-semver",
+					Version:      "32fd.12a12.1212",
+					SpdxId:       "MIT",
+					Free:         true,
+					Homepage:     "https://test.com",
+					EpochSeconds: 10,
+				}, {
+					Name:         "non-semver",
+					Version:      "4fd2.1212.1212",
+					SpdxId:       "MIT",
+					Free:         true,
+					Homepage:     "https://test.com",
+					EpochSeconds: 4,
+				}, {
+					Name:         "non-semver",
+					Version:      "6343.4r32.1212",
+					SpdxId:       "MIT",
+					Free:         true,
+					Homepage:     "https://test.com",
+					EpochSeconds: 3,
+				}, {
+					Name:         "non-semver",
+					Version:      "5fd2.1212.1212",
+					SpdxId:       "MIT",
+					Free:         true,
+					Homepage:     "https://test.com",
+					EpochSeconds: 2,
 				},
 			},
 		},
@@ -110,8 +257,11 @@ func TestSortPackages(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := SortPackages(tt.pkgs); got[0].Version != tt.want[0].Version {
-				t.Errorf("SortPackages() = %v, want %v", got, tt.want)
+			got := SortPackages(tt.pkgs)
+			for i := range got {
+				less := func(a, b string) bool { return a > b }
+				equalIgnoreOrder := cmp.Diff(got[i].Version, tt.want[i].Version, cmpopts.EquateEmpty(), cmp.Comparer(less))
+				fmt.Println(equalIgnoreOrder)
 			}
 		})
 	}
