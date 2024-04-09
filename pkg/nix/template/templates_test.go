de05b68b8727db5708d30a08f6f099fa4d707a04
Rakshit <rakshitgondwal3@gmail.com>
2024-04-09 21:29:07 +0530

    add test cases for template
    Signed-off-by: Rakshit <rakshitgondwal3@gmail.com>

--- pkg/nix/template/templates_test.go
+++ pkg/nix/template/templates_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/buildsafedev/bsf/pkg/hcl2nix"
 )
 
-func TestTemplateMain(t *testing.T) {
+func TestTemplateMainForGolang(t *testing.T) {
 
 	flake := Flake{
 		Description: "Simple flake",
@@ -24,7 +24,13 @@ func TestTemplateMain(t *testing.T) {
 		},
 	}
 
-	err := GenerateFlake(flake, os.Stdout, nil)
+	conf := &hcl2nix.Config{
+		GoModule: &hcl2nix.GoModule{
+			Name: "go-project",
+		},
+	}
+
+	err := GenerateFlake(flake, os.Stdout, conf)
 	if err != nil {
 		t.Error()
 		t.FailNow()
@@ -52,6 +58,36 @@ func TestTemplateMainForRust(t *testing.T) {
 		RustApp: &hcl2nix.RustApp{
 			RustVersion: "1.25.0",
 			Release:     true,
+		},
+	}
+
+	err := GenerateFlake(flake, os.Stdout, conf)
+	if err != nil {
+		t.Error()
+		t.FailNow()
+	}
+}
+
+func TestTemplateMainForPython(t *testing.T) {
+
+	flake := Flake{
+		Description: "Simple flake",
+		NixPackageRevisions: []string{
+			"a89ba043dda559ebc57fc6f1fa8cf3a0b207f688",
+			"a9bf124c46ef298113270b1f84a164865987a91c",
+		},
+		DevPackages: map[string]string{
+			"gotools": "a89ba043dda559ebc57fc6f1fa8cf3a0b207f688",
+			"go_1_19": "a89ba043dda559ebc57fc6f1fa8cf3a0b207f688",
+		},
+		RuntimePackages: map[string]string{
+			"bash": "a9bf124c46ef298113270b1f84a164865987a91c",
+		},
+	}
+
+	conf := &hcl2nix.Config{
+		PoetryApp: &hcl2nix.PoetryApp{
+			ProjectDir: "../.",
 		},
 	}
 
