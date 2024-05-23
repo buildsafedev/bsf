7df99bbc216009adb1cf342bbf5631405856c885
Rakshit Gondwal <rakshitgondwal3@gmail.com>
2024-05-23 13:50:33 +0530

    modify logic
    Signed-off-by: Rakshit Gondwal <rakshitgondwal3@gmail.com>

--- pkg/attestation/attestation.go
+++ pkg/attestation/attestation.go
@@ -10,29 +10,19 @@ import (
 	intoto "github.com/in-toto/in-toto-golang/in_toto"
 )
 
-var ValidPreds = map[string][]string{
-	"https://slsa.dev/provenance/":                         {"SLSA Provenance", "Provenance"},
-	"https://in-toto.io/attestation/vulns":                 {"Vulnerability", "vuln"},
-	"https://slsa.dev/verification_summary/v1":             {"SLSA Verification Summary", "vsa"},
-	"https://in-toto.io/attestation/test-result/v0.1":      {"Test Result", "test-result"},
-	"https://spdx.dev/Document":                            {"SPDX"},
-	"https://spdx.github.io/spdx-spec":                     {"SPDX"},
-	"https://in-toto.io/attestation/scai/attribute-report": {"SCAI Report", "scai"},
-	"https://in-toto.io/attestation/runtime-trace/":        {"Runtime Traces", "runtime-trace"},
-	"https://in-toto.io/attestation/release":               {"Release"},
-	"https://in-toto.io/attestation/link":                  {"Link"},
-	"https://cyclonedx.org/bom":                            {"CycloneDX", "CDX"},
-	"https://cyclonedx.org/specification/overview/":        {"CycloneDX", "CDX"},
-}
-
-var predicateURIs = []string{
-	"https://slsa.dev/provenance/",
-	"https://in-toto.io/attestation/",
-	"https://slsa.dev/verification_summary/v1",
-	"https://spdx.github.io/spdx-spec/v2.3/",
-	"https://spdx.dev/Document",
-	"https://cyclonedx.org/specification/overview/",
-	"https://cyclonedx.org/bom",
+var ValidPreds = map[string]string{
+	"https://slsa.dev/provenance/":                         "provenance",
+	"https://in-toto.io/attestation/vulns":                 "vuln",
+	"https://slsa.dev/verification_summary/v1":             "vsa",
+	"https://in-toto.io/attestation/test-result/":          "test-result",
+	"https://spdx.dev/Document":                            "spdx",
+	"https://spdx.github.io/spdx-spec":                     "spdx",
+	"https://in-toto.io/attestation/scai/attribute-report": "scai",
+	"https://in-toto.io/attestation/runtime-trace/":        "runtime-trace",
+	"https://in-toto.io/attestation/release":               "release",
+	"https://in-toto.io/attestation/link":                  "link",
+	"https://cyclonedx.org/bom":                            "cdx",
+	"https://cyclonedx.org/specification/overview/":        "cdx",
 }
 
 func ValidateInTotoStatement(file []byte) (map[string][]intoto.Statement, error) {
@@ -46,7 +36,8 @@ func ValidateInTotoStatement(file []byte) (map[string][]intoto.Statement, error)
 			return nil, fmt.Errorf("invalid JSON: %v", err)
 		}
 
-		if err := validatePredicateType(statement.StatementHeader); err != nil {
+		gotPredType, err := validatePredicateType(statement.StatementHeader)
+		if err != nil {
 			return nil, err
 		}
 
@@ -59,7 +50,7 @@ func ValidateInTotoStatement(file []byte) (map[string][]intoto.Statement, error)
 				return nil, fmt.Errorf("subject name is empty")
 			}
 		}
-		predStatementMap[statement.PredicateType] = append(predStatementMap[statement.PredicateType], statement)
+		predStatementMap[gotPredType] = append(predStatementMap[gotPredType], statement)
 
 	}
 	if err := scanner.Err(); err != nil {
@@ -68,48 +59,58 @@ func ValidateInTotoStatement(file []byte) (map[string][]intoto.Statement, error)
 	return predStatementMap, nil
 }
 
-func validatePredicateType(statement intoto.StatementHeader) error {
+func validatePredicateType(statement intoto.StatementHeader) (string, error) {
 
 	if !strings.Contains("https://in-toto.io/Statement/v1", statement.Type) {
-		return fmt.Errorf("invalid _type: %s", statement.Type)
+		return "", fmt.Errorf("invalid _type: %s", statement.Type)
 	}
 
 	if statement.PredicateType == "" {
-		return fmt.Errorf("predicateType is empty")
+		return "", fmt.Errorf("predicateType is empty")
 	}
 
-	for _, uri := range predicateURIs {
-		if strings.Contains(statement.PredicateType, uri) {
-			return nil
+	for pred, shortName := range ValidPreds {
+		if strings.Contains(statement.PredicateType, pred) {
+			return shortName, nil
 		}
 	}
 
-	return fmt.Errorf("predicateType %s is invalid", statement.PredicateType)
+	return "", fmt.Errorf("predicateType %s is invalid", statement.PredicateType)
 }
 
-func GetPredicate(psMap map[string][]intoto.Statement, predtype string, subject string) ([]intoto.Statement) {
+func GetPredicate(psMap map[string][]intoto.Statement, predtype string, subject string) []intoto.Statement {
+	// There can be multiple valid keys for the predType.
+	// Such as, for spdx value, we have multiple keys.
+	// Once we store them in a slice, we can later loop
+	// over this to find the key that matches ps
 	var keysToFind []string
+	// If a user does not provides a subject, there can be
+	// scenarios when multiple preds of same type can be 
+	// available. This slice is going to store all of them.
 	var predsFound []intoto.Statement
 
-	// Find the key in ValidPreds based on the predtype
-	for key, values := range ValidPreds {
-		for _, value := range values {
-			if strings.EqualFold(value, predtype) {
-				keysToFind = append(keysToFind, key)
-				break
-			}
+	// Find the preds in ValidPreds based on the predtype
+	for key, value := range ValidPreds {
+		if strings.EqualFold(value, predtype) {
+			keysToFind = append(keysToFind, key)
+			break
 		}
 	}
 
-	// Loop over the psMap and find the matching key
-	for key, statements := range psMap {
-		for _, keyToFind := range keysToFind {
-			if strings.Contains(key, keyToFind) {
-				for _, statement := range statements {
+	// 1. Loop over the psMap to first fetch all intoto.Statement
+	// 2. Loop over keysToFind to check if the key and statement match
+	// 3. Check if subject is defined, if yes, match the subject.
+	// 4. If the subject is found, append the statement to predsFound and break.
+	// 5. If subject is not found, keep appending the matched statement to predsFound.
+	for _, statements := range psMap {
+		for _, statement := range statements {
+			for _, keyToFind := range keysToFind {
+				if strings.Contains(statement.PredicateType, keyToFind) {
 					if subject != "" {
 						for _, predSub := range statement.StatementHeader.Subject {
 							if predSub.Name == subject {
 								predsFound = append(predsFound, statement)
+								break
 							}
 						}
 					} else {
