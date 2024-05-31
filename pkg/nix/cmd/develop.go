e0b4cdd07286aa9052d8e20a9d97438443e76780
Praful Khanduri <99384392+Horiodino@users.noreply.github.com>
2024-05-31 14:52:19 +0530

    shell preference (#1)
    * shell preference
    
    Signed-off-by: Horiodino <holiodin@gmail.com>

--- pkg/nix/cmd/develop.go
+++ pkg/nix/cmd/develop.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 // Develop opens a BSF development shell
@@ -12,8 +13,26 @@ func Develop() error {
 	if err != nil {
 		return err
 	}
+
+	var cmd *exec.Cmd
+	var shells []string
+	shell := os.Getenv("SHELL")
+
 	// using the `path:` will let users work on the project without having to interact with git
-	cmd := exec.Command("nix", "develop", fmt.Sprintf("path:%s/bsf/.#devShell", dir))
+	if shell == "" {
+		cmd = exec.Command("nix", "develop", fmt.Sprintf("path:%s/bsf/.#devShell", dir))
+	} else {
+		if strings.Contains(shell, " ") {
+			shells = strings.Split(shell, " ")
+		} else if strings.Contains(shell, ":") {
+			shells = strings.Split(shell, ":")
+		} else {
+			shells = strings.Split(shell, ";")
+		}
+
+		cmd = exec.Command("nix", "develop", fmt.Sprintf("path:%s/bsf/.#devShell", dir), "-c", shells[0])
+
+	}
 	// Connect the command's stdin, stdout, and stderr to the terminal
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
