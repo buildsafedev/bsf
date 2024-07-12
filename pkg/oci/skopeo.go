16589a41df22470d53bfb22a3650c06145a8cf06
Horiodino <holiodin@gmail.com>
2024-07-12 14:16:56 +0530

    refactor
    Signed-off-by: Horiodino <holiodin@gmail.com>

--- pkg/oci/skopeo.go
+++ pkg/oci/skopeo.go
@@ -7,8 +7,6 @@ import (
 	"os/user"
 )
 
-var registries = []string{"docker.io"}
-
 // LoadDocker loads the image to the docker daemon
 func LoadDocker(daemon, dir, imageName string) error {
 	cmd := exec.Command("nix", "run", "nixpkgs#skopeo", "--", "copy", "--insecure-policy", "--dest-daemon-host="+daemon, "dir:"+dir, "docker-daemon:"+imageName)
@@ -54,27 +52,20 @@ func Push(dir, imageName string) error {
 }
 func Auth(registryName string) error {
 
-	for _, reg := range registries {
-		if reg == registryName {
-			currentuser, err := user.Current()
-			if err != nil {
-				return err
-			}
-			fmt.Println("Logging in to registry")
-			authFile := currentuser.HomeDir + "/.skopeo/config.json"
-			cmd := exec.Command("nix", "run", "nixpkgs#skopeo", "--", "login", "--authfile", authFile, registryName)
-			cmd.Stdin = os.Stdin
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-
-			err = cmd.Run()
-			if err != nil {
-				return err
-			}
-			return nil
-		}
-
+	currentuser, err := user.Current()
+	if err != nil {
+		return err
 	}
+	fmt.Println("Logging in to registry")
+	authFile := currentuser.HomeDir + "/.skopeo/config.json"
+	cmd := exec.Command("nix", "run", "nixpkgs#skopeo", "--", "login", "--authfile", authFile, registryName)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 
-	return fmt.Errorf("Registry not found")
+	err = cmd.Run()
+	if err != nil {
+		return err
+	}
+	return nil
 }
