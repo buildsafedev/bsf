2b6d852b285bf2bda9c5f8b7d2f75b6559d6c4ed
Horiodino <holiodin@gmail.com>
2024-11-12 13:07:44 +0530

    fix --df-swap functionality for bsf oci  #119
    Signed-off-by: Horiodino <holiodin@gmail.com>

--- pkg/builddocker/build_test.go
+++ pkg/builddocker/build_test.go
@@ -8,58 +8,44 @@ import (
 
 func TestEditDockerFile(t *testing.T) {
 	tests := []struct {
-		name        string
-		lines       []string
-		isDev       bool
-		tag         string
-		expectedRes []string
-		expectError bool
+		ociArtifactName string
+		name            string
+		lines           []string
+		tag             string
+		expectedRes     []string
+		expectError     bool
 	}{
 		{
-			name: "Dev",
+			name:            "FROM Command with bsf tag",
+			ociArtifactName: "docker.io/buildsafe/bsf",
 			lines: []string{
-				"FROM ubuntu:18.04 # bsfimage:dev",
+				"FROM docker.io/buildsafe/bsf:v1.1.1 AS build",
 				"RUN apt-get update",
 			},
-			isDev: true,
-			tag:   "latest",
+			tag: "latest",
 			expectedRes: []string{
-				"FROM ubuntu:latest # bsfimage:dev",
+				"FROM docker.io/buildsafe/bsf:latest AS build",
 				"RUN apt-get update",
 			},
 			expectError: false,
 		},
 		{
-			name: "Runtime",
-			lines: []string{
-				"FROM ubuntu:18.04 # bsfimage:runtime",
-				"RUN apt-get update",
-			},
-			isDev: false,
-			tag:   "latest",
-			expectedRes: []string{
-				"FROM ubuntu:latest # bsfimage:runtime",
-				"RUN apt-get update",
-			},
-			expectError: false,
-		},
-		{
-			name: "No FROM Command with bsf tag",
+			name:            "No FROM Command with bsf tag",
+			ociArtifactName: "docker.io/buildsafe/bsf",
 			lines: []string{
 				"FROM ubuntu:latest",
 				"RUN apt-get update",
 			},
-			isDev:       true,
 			tag:         "latest",
 			expectedRes: nil,
 			expectError: true,
 		},
 		{
-			name: "No FROM Command",
+			name:            "No FROM Command",
+			ociArtifactName: "docker.io/buildsafe/bsf",
 			lines: []string{
 				"RUN apt-get update",
 			},
-			isDev:       true,
 			tag:         "latest",
 			expectedRes: nil,
 			expectError: true,
@@ -68,7 +54,7 @@ func TestEditDockerFile(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			res, err := editDockerfile(tt.lines, tt.isDev, tt.tag)
+			res, err := editDockerfile(tt.lines, tt.ociArtifactName, tt.tag)
 			if tt.expectError {
 				assert.Error(t, err)
 			} else {
